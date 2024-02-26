68e62e9677cc4721edbd8c5d3b04e4bdd56e3ef6
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-02-26 21:49:58 +0900

    Init

---
+++ URLSession_Alamofire/URLSession_Alamofire/URLSession_AlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSession_AlamofireApp.swift
+//  URLSession_Alamofire
+//
+//  Created by 승재 on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct URLSession_AlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
